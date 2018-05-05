02af9f17cad1a6296959426302b8ac6abdcd958d
Apo45ty <antonio.tapia@upr.edu>
2018-05-04 22:23:41 -0400

    Added open chrome goto salesforce and login

---
+++ src/test/java/com/revature/selenium/Selenium.java
@@ -0,0 +1,39 @@
+package com.revature.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+	
+	WebDriver driver;
+	public void invokeBrowser() {
+		System.setProperty("webdriver.chrome.driver","src\\test\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println(driver);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		driver.get("https://dev.assignforce.revaturelabs.com");
+		
+		login();
+	}
+	
+	public void login() {
+		driver.findElement(By.id("username")).sendKeys("");;
+		driver.findElement(By.id("password")).sendKeys("");
+		driver.findElement(By.id("Login")).click();;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		Selenium s = new Selenium();
+		s.invokeBrowser();
+		
+	}
+}
