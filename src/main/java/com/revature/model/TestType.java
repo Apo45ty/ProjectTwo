10a4bc567e98139c6a25da681beb183abec15d3f
Ramnery Lugo <ramnerylugo@Ramnerys-MacBook-Pro.local>
2018-05-07 15:25:01 -0400

    Added foreign key.

--- src/main/java/com/revature/model/TestType.java
+++ src/main/java/com/revature/model/TestType.java
@@ -5,10 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
 
 @XmlRootElement
 @Entity
@@ -33,6 +36,10 @@ public class TestType {
 		this.group = group;
 		this.isDeleted = isDeleted;
 	}
+	@ManyToOne
+	@JoinColumn (name = "groupID")
+	TestModel testmodel = new TestModel();
+
 	@XmlElement
 	public long getId() {
 		return id;
