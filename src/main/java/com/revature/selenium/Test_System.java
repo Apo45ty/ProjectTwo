bb6ea06f284a4944d3a422dbaaf389629e71fc0a
Kevin Rivera <kevinriveraut@gmail.com>
2018-05-11 16:45:13 -0400

    testing create method of hibernate

--- src/main/java/com/revature/selenium/Test_System.java
+++ src/main/java/com/revature/selenium/Test_System.java
@@ -6,19 +6,26 @@ import java.util.Set;
 
 import com.revature.db.DatabaseSingletonDao;
 import com.revature.db.DatabaseSingletonDaoImpl;
+import com.revature.model.Test;
 import com.revature.model.TestSystem;
+import com.revature.model.TestType;
 
 public class Test_System {
     public static void main(String[] args) {
 		DatabaseSingletonDao dao = DatabaseSingletonDaoImpl.getInstance();
 		
-		//Get the required system information
-		long maxMemory = Runtime.getRuntime().maxMemory();
-        String OS = System.getProperty("os.name");
-		String cpu = System.getenv("PROCESSOR_IDENTIFIER");
-        File[] roots = File.listRoots();
+		TestSystem thisSiS = new TestSystem(111L,"CPU!!!","OS!!!","RAMMM!!!","DisK Drive Info ALL TEMP");
+		 	TestType temp0 = new TestType(4L,"test",3L,false);
+			 Test sendit = new Test(5L,"test","Failed",false,temp0,thisSiS);
+			 dao.create(sendit);
 
-		TestSystem sys = new TestSystem(0, cpu, OS, (maxMemory + " bytes"), (roots[0].getTotalSpace() + " bytes"));
-		dao.createSys(sys);
+		// //Get the required system information
+		// long maxMemory = Runtime.getRuntime().maxMemory();
+        // String OS = System.getProperty("os.name");
+		// String cpu = System.getenv("PROCESSOR_IDENTIFIER");
+        // File[] roots = File.listRoots();
+
+		// TestSystem sys = new TestSystem(0, cpu, OS, (maxMemory + " bytes"), (roots[0].getTotalSpace() + " bytes"));
+		// dao.createSys(sys);
     }
 }
