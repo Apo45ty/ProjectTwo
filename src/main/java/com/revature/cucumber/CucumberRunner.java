39a2638cde06b4fac247c01097a904ef36584d01
Francisco Lugo <francisco.alugojimenez@gmail.com>
2018-05-11 15:06:31 -0400

    added Cucumber files
    CucumberRunner, StepDefinition in src/main/java
    and
    cucumberFeature.feature in src/main/resources

---
+++ src/main/java/com/revature/cucumber/CucumberRunner.java
@@ -0,0 +1,9 @@
+package com.revature.cucumber;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features="C:\\revature\\workspace\\CucumberAndSelenium\\src\\test\\resources\\com\\cucumber\\feature")
+public class CucumberRunner extends AbstractTestNGCucumberTests{
+
+}
