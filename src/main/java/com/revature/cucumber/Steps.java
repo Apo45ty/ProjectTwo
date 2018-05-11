11ffed15db59b297d4b77f326ce6a76cde69a9fb
Ramnery Lugo <ramnerylugo@Ramnerys-MacBook-Pro.local>
2018-05-11 11:55:52 -0400

    Delay

--- src/main/java/com/revature/cucumber/Steps.java
+++ src/main/java/com/revature/cucumber/Steps.java
@@ -3,6 +3,7 @@ package com.revature.cucumber;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
