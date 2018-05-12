d8c1dc1c88ad4e247293163e5b62d6048c4f96c0
Kevin Rivera <kevinriveraut@gmail.com>
2018-05-12 19:14:04 -0400

    Fixed an error with the Ignore import

--- src/test/java/com/revature/test/Trainer_Locations_Steps.java
+++ src/test/java/com/revature/test/Trainer_Locations_Steps.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Ignore;
+// import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 //@Listeners(SafeForceResultListener.class)
