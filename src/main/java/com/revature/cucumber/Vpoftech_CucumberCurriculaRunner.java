bad3ce90a028fd4e57c51522933baac5a47a369e
Ramnery Lugo <ramnerylugo@Ramnerys-MacBook-Pro.local>
2018-05-12 18:26:17 -0400

    Nuevos cambios

---
+++ src/main/java/com/revature/cucumber/Vpoftech_CucumberCurriculaRunner.java
@@ -0,0 +1,9 @@
+package com.revature.cucumber;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features="src/main/resources/vpoftech_CucumberCurricula.feature")
+public class Vpoftech_CucumberCurriculaRunner extends AbstractTestNGCucumberTests {
+
+}
--- src/test/java/com/revature/test/StepsCurricula.java
+++ src/test/java/com/revature/test/StepsCurricula.java
@@ -1,13 +1,10 @@
-package com.revature.cucumber;
+package com.revature.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,22 +12,25 @@ public class StepsCurricula {
 
     WebDriver driver;
 
-    @Given("^Launches Chrome Browser and initiates login$")
-    public void launches_Chrome_Browser_and_initiates_login() throws Throwable {
+//            ("^Launches Chrome Browser and initiates login$")
+@Test
+public void launches_Chrome_Browser_and_initiates_login() throws Throwable {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://dev.assignforce.revaturelabs.com");
     }
 
-    @When("^The Trainer User enters the \"([^\"]*)\" and \"([^\"]*)\"$")
-    public void the_Trainer_User_enters_the_and(String username, String password) throws Throwable {
+//    @When("^The Trainer User enters the \"([^\"]*)\" and \"([^\"]*)\"$")
+@Test
+public void the_Trainer_User_enters_the_and(String username, String password) throws Throwable {
         driver.findElement(By.id("username")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("password")).sendKeys(Keys.RETURN);
     }
 
-    @Then("^Clicks on curricula tabs and navigates$")
-    public void clicks_on_curricula_tabs_and_navigates() throws Throwable {
+//    @Then("^Clicks on curricula tabs and navigates$")
+@Test
+public void clicks_on_curricula_tabs_and_navigates() throws Throwable {
         TimeUnit.SECONDS.sleep(8);
         driver.findElement(By.xpath("/html/body/div/div[1]/ng-include/div/md-content/md-nav-bar/div/nav/ul/li[4]/a/span/span")).click();
         driver.findElement(By.id("coreArrow")).click();
@@ -41,7 +41,8 @@ public class StepsCurricula {
     }
 
 
-@Then("^Quits session right after testing curricula$")
+//@Then("^Quits session right after testing curricula$")
+@Test
 public void quits_session_right_after_testing_curricula() throws Throwable {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         TimeUnit.SECONDS.sleep(8);
