8689a35fc38dd97c4717334c8bfc60396d31c3b4
Ed-Mar <35546470+Ed-Mar@users.noreply.github.com>
2018-05-13 17:41:34 -0400

    Messing with Listeners

--- src/main/java/com/revature/cucumber/Trainer_Locations_Runner.java
+++ src/main/java/com/revature/cucumber/Trainer_Locations_Runner.java
@@ -6,7 +6,7 @@ import com.revature.test.SafeForceResultListener;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-
+@Listeners(SafeForceResultListener.class)
 @CucumberOptions(features="src/main/resources/trainer_locations.feature")
 public class Trainer_Locations_Runner extends AbstractTestNGCucumberTests{
 
