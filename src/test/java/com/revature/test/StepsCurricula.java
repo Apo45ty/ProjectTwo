f09842a49db16f1706aa57a82098c69dc580a638
Ramnery Lugo <ramnerylugo@Ramnerys-MacBook-Pro.local>
2018-05-13 12:10:10 -0400

    Added priority

--- src/test/java/com/revature/test/StepsCurricula.java
+++ src/test/java/com/revature/test/StepsCurricula.java
@@ -12,24 +12,19 @@ public class StepsCurricula {
 
     WebDriver driver;
 
-//            ("^Launches Chrome Browser and initiates login$")
-@Test
+@Test (priority = 1)
 public void launches_Chrome_Browser_and_initiates_login() throws Throwable {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://dev.assignforce.revaturelabs.com");
-    }
-
-//    @When("^The Trainer User enters the \"([^\"]*)\" and \"([^\"]*)\"$")
-@Test
-public void the_Trainer_User_enters_the_and(String username, String password) throws Throwable {
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("username")).sendKeys("test.trainer@revature.com.int1");
+        driver.findElement(By.id("password")).sendKeys("trainer123");
         driver.findElement(By.id("password")).sendKeys(Keys.RETURN);
     }
+    
 
 //    @Then("^Clicks on curricula tabs and navigates$")
-@Test
+@Test (priority = 2)
 public void clicks_on_curricula_tabs_and_navigates() throws Throwable {
         TimeUnit.SECONDS.sleep(8);
         driver.findElement(By.xpath("/html/body/div/div[1]/ng-include/div/md-content/md-nav-bar/div/nav/ul/li[4]/a/span/span")).click();
@@ -42,7 +37,7 @@ public void clicks_on_curricula_tabs_and_navigates() throws Throwable {
 
 
 //@Then("^Quits session right after testing curricula$")
-@Test
+@Test (priority = 3)
 public void quits_session_right_after_testing_curricula() throws Throwable {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         TimeUnit.SECONDS.sleep(8);
