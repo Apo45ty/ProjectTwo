fd9d8bbd8cb458275b6373827e9741dd366c1d8e
Ed-Mar <35546470+Ed-Mar@users.noreply.github.com>
2018-05-14 16:20:31 -0400

    Add grouping and dependpency for test and add impleict waits for clicks

--- src/main/java/com/revature/test/VP_Click_Test_Sutie.java
+++ src/main/java/com/revature/test/VP_Click_Test_Sutie.java
@@ -3,220 +3,213 @@ package com.revature.test;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import com.revature.pom.BatchesPOM;
 import com.revature.pom.LoginPage;
 import com.revature.pom.NavbarPOM;
 
 @Listeners(SafeForceResultListener.class)
-public class Test_Runner {
+public class VP_Click_Test_Sutie {
 
 	static WebDriver wd;
 
 	Thread thread;
-	
- 
- 
-	/**
-	 * 
-	 */
-	// @Test(priority = 0)
-	// public void VerifyValidLogin() {
-	// openApplication();
-	// LoginPage.user(wd).sendKeys("test.trainer@revature.com.int1");
-	// LoginPage.pass(wd).sendKeys("trainer123");
-	// LoginPage.loginButton(wd).click();
-	// wd.manage().timeouts().implicitlyWait(33, TimeUnit.SECONDS);
-	// navbar.logout(wd).click();
-	// wd.manage().timeouts().implicitlyWait(33, TimeUnit.SECONDS);
-	//
-	// }
+	private int howLongtoWaitInSeconds = 3;
+	@Test(groups = { "VP_Login" })
+	public void LoginAsVP() {
 
-//	@Test(groups = { "include-test-one" })
-//
-//	public void LoginAsVP() {
-//		
-//		try {
-//			loginAsVPofTech();
-//			TimeUnit.SECONDS.sleep(5);
-//		} catch (Exception e) {
-//			System.out.println("I am guessing the log in failed");
-//			org.testng.Assert.fail("To Login in");
-//		} finally {
-//			wd.quit();
-//		}
-//	}
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksOverview() {
-//	 try {
-//	 loginAsVPofTech();
-//	 
-//	 clickCurricula();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksBatches() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickBatches();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksCurricula() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickCurricula();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksLocation() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickLocations();
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksTraniner() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickTrainers();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksProfile() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickProfile();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksSettings() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickSettings();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
-//	
-//	 @Test(groups = { "include-test-one" })
-//	 public void VPclicksLogout() {
-//	 try {
-//	 loginAsVPofTech();
-//	 clickLogout();
-//	 TimeUnit.SECONDS.sleep(5);
-//	 } catch (Exception e) {
-//	 System.out.println("I am guessing the log in failed");
-//	 org.testng.Assert.fail("Failed to Click");
-//	 } finally {
-//	 wd.quit();
-//	 }
-//	
-//	 }
+		try {
+			loginAsVPofTech();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Login");
+		} finally {
+			wd.quit();
+		}
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksOverview() {
+		try {
+			loginAsVPofTech();
+
+			clickCurricula();
+			TimeUnit.SECONDS.sleep(5);
+			wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksBatches() {
+		try {
+			loginAsVPofTech();
+			clickBatches();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksCurricula() {
+		try {
+			loginAsVPofTech();
+			clickCurricula();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksLocation() {
+		try {
+			loginAsVPofTech();
+			clickLocations();
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksTraniner() {
+		try {
+			loginAsVPofTech();
+			clickTrainers();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksProfile() {
+		try {
+			loginAsVPofTech();
+			clickProfile();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksSettings() {
+		try {
+			loginAsVPofTech();
+			clickSettings();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
+
+	@Test(groups = { "VP_Login", "Click" }, dependsOnMethods = { "LoginAsVP" })
+	public void VPclicksLogout() {
+		try {
+			loginAsVPofTech();
+			clickLogout();
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			System.out.println("I am guessing the log in failed");
+			org.testng.Assert.fail("Failed to Click");
+		} finally {
+			wd.quit();
+		}
+
+	}
 
 	public void waitASecound() {
 
 	}
 
 	public void clickOverview() {
-		WebDriverWait wait = new WebDriverWait(wd, 10);
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.overview(wd)));
 		element.click();
-		
+
 	}
 
 	public void clickBatches() {
-		WebDriverWait wait = new WebDriverWait(wd, 10);
+		WebDriverWait wait = new WebDriverWait(wd, 1howLongtoWaitInSeconds);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.batches(wd)));
 		element.click();
 	}
 
 	public void clickLocations() {
-		WebDriverWait wait = new WebDriverWait(wd, 3);
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.locations(wd)));
 		element.click();
 	}
 
 	public void clickCurricula() {
-		NavbarPOM.curricula(wd).click();
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.curricula(wd)));
+		
+		
 	}
 
 	public void clickTrainers() {
-		NavbarPOM.trainers(wd).click();
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.trainers(wd)));
+		element.click();
 	}
 
 	public void clickProfile() {
-		NavbarPOM.profile(wd).click();
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.profile(wd)));
+		element.click();
+		
 	}
 
 	public void clickSettings() {
-		NavbarPOM.settings(wd).click();
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.settings(wd)));
+		element.click();
+		
 	}
 
 	public void clickLogout() {
-		NavbarPOM.logout(wd).click();
+		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.settings(wd)));
+		element.click();
+		
 	}
 
 	// Actions builder = new Actions(wd);
