8cbd7123eeb2ff59b043dca09f8d8b8e2b74cf8f
Ed-Mar <35546470+Ed-Mar@users.noreply.github.com>
2018-05-15 14:01:39 -0400

    DropDown Activation Complete, Need to Test Results

--- src/main/java/com/revature/test/VP_Overview_Suite.java
+++ src/main/java/com/revature/test/VP_Overview_Suite.java
@@ -24,61 +24,136 @@ public class VP_Overview_Suite {
 	static WebDriver wd;
 	private int howLongtoWaitInSeconds = 3;
 	private static String downloadPath = "C:\\Users\\kaiser\\Downloads";
-//	@Test(groups = { "VP_Login" })
-//	public void LoginAsVP() {
-//
-//		try {
-//			loginAsVPofTech();
-//			TimeUnit.SECONDS.sleep(2);
-//		} catch (Exception e) {
-//			System.out.println("I am guessing the log in failed");
-//			org.testng.Assert.fail("Failed to Login");
-//		} finally {
-//			wd.quit();
-//		}
-//	}
-//
-//	@Test(groups = { "VP_Login", "Click", "Lvl=01" }, dependsOnMethods = { "LoginAsVP" })
-//	public void VPclicksOverview() {
-//		try {
-//			loginAsVPofTech();
-//			clickOverview();
-//			TimeUnit.SECONDS.sleep(2);
-//			wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//		} catch (Exception e) {
-//			System.out.println("I am guessing the log in failed");
-//			org.testng.Assert.fail("Failed to Click");
-//		} finally {
-//			wd.quit();
-//		}
-//	}
+	// @Test(groups = { "VP_Login" })
+	// public void LoginAsVP() {
+	//
+	// try {
+	// loginAsVPofTech();
+	// TimeUnit.SECONDS.sleep(2);
+	// } catch (Exception e) {
+	// System.out.println("I am guessing the log in failed");
+	// org.testng.Assert.fail("Failed to Login");
+	// } finally {
+	// wd.quit();
+	// }
+	// }
+	//
+	// @Test(groups = { "VP_Login", "Click", "Lvl=01" }, dependsOnMethods = {
+	// "LoginAsVP" })
+	// public void VPclicksOverview() {
+	// try {
+	// loginAsVPofTech();
+	// clickOverview();
+	// TimeUnit.SECONDS.sleep(2);
+	// wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	// } catch (Exception e) {
+	// System.out.println("I am guessing the log in failed");
+	// org.testng.Assert.fail("Failed to Click");
+	// } finally {
+	// wd.quit();
+	// }
+	// }
 
 	// commented out for quicker testig and study
-	//TODO replace hard waits for soft one
-	@Test(groups = { "VP_Login", "Click", "Overview", "Happy" })
-	//, dependsOnMethods = { "LoginAsVP", "VPclicksOverview" })
-	public void VPOverviewClickExportCSV() {
-		String expectedFileNameToBeDL = "batches.csv";
-		
+	// TODO replace hard waits for soft one
+	// @Test(groups = { "VP_Login", "Click", "Overview", "Happy" })
+	// // , dependsOnMethods = { "LoginAsVP", "VPclicksOverview" })
+	// public void VPOverviewClickExportCSV() {
+	// String expectedFileNameToBeDL = "batches.csv";
+	//
+	// try {
+	// FileUtil.DeleteFile(downloadPath, expectedFileNameToBeDL);// Deletes The File
+	// expected to be downloaded if
+	// // it is already there
+	// loginAsVPofTech();// login
+	// TimeUnit.SECONDS.sleep(5); // Wait for page to load
+	// wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // really making
+	// sure the page has loaded
+	// try {
+	// TimeUnit.SECONDS.sleep(2); // Wait for page to load
+	// WebElement overview_getCSV =
+	// wd.findElement(By.cssSelector("button[filename='batches.csv']"));
+	// System.out.println(overview_getCSV.toString());
+	// overview_getCSV.click(); // click on the found element I hope
+	// TimeUnit.SECONDS.sleep(3);// Apperantly you need to wait for the download
+	// Assert.assertTrue(FileUtil.isFileThere(downloadPath, expectedFileNameToBeDL),
+	// "no filename matches anything in the given dierctory");
+	// } catch (NullPointerException e) {
+	// org.testng.Assert.fail("Get CSV for Overview has Failed to appear in
+	// Document");
+	// System.out.println("Failed to find Eelement \'overview_getCSV\'");
+	//
+	// }
+	//
+	// TimeUnit.SECONDS.sleep(4);
+	// } catch (Exception e) {
+	// System.out.println("Failure due to: " + e.getLocalizedMessage());
+	// org.testng.Assert.fail("Failed to Click");
+	// } finally {
+	// wd.quit();
+	// }
+	// }
+
+	@Test(groups = { "VP_Login", "Click", "Overview", "Happy", "DropDown" })
+	// , dependsOnMethods = { "LoginAsVP", "VPclicksOverview" })
+	public void VPOverviewFilter() {
+
 		try {
-			FileUtil.DeleteFile(downloadPath, expectedFileNameToBeDL);// Deletes The File expected to be downloaded if it is already there
-			loginAsVPofTech();// login 
-			TimeUnit.SECONDS.sleep(5); // Wait for page to load
-			wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //really making sure the page has loaded
+			loginAsVPofTech();// login
+			TimeUnit.SECONDS.sleep(3); // Wait for page to load
+			wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // really making sure the page has loaded
 			try {
 				TimeUnit.SECONDS.sleep(2); // Wait for page to load
-				WebElement overview_getCSV = wd.findElement(By.cssSelector("button[filename='batches.csv']"));
-				System.out.println(overview_getCSV.toString());
-				overview_getCSV.click(); // click on the found element I hope
-				TimeUnit.SECONDS.sleep(3);// Apperantly you need to wait for the download	
-				Assert.assertTrue(FileUtil.isFileThere(downloadPath, expectedFileNameToBeDL), "no filename matches anything in the given dierctory");
+				// Finds the Filter Button I hope
+				WebElement overview_element_filter_btn = wd.findElement(By.cssSelector("button[aria-label='Filter']"));
+				System.out.println(overview_element_filter_btn.getTagName().toString());
+				overview_element_filter_btn.click(); // click on the found element I hope
+				TimeUnit.MILLISECONDS.sleep(200);
+
+				// Progress DropDown Option
+				WebElement overview_element_filter_Drop_Active = wd
+						.findElement(By.cssSelector("button[ng-click=\"hCtrl.batchFilter = 'Active'\"]"));
+				TimeUnit.MILLISECONDS.sleep(100);
+				System.out.println(overview_element_filter_Drop_Active.getTagName().toString());
+				overview_element_filter_Drop_Active.click();
+				TimeUnit.MILLISECONDS.sleep(350);
+
+				/****
+				 * NOTE TO FUTURE ED REMEBER TO CLICK THE DROP (filter) DOWN AGAIN TO BE ABLE TO
+				 * CLICK THINGS IN IT(think about it you wasted 30mins on this)vvv
+				 ****/
+				//Re Clicks the Filter Button for the dropdown
+				overview_element_filter_btn.click();
+				TimeUnit.MILLISECONDS.sleep(200);
+
+				// Beginning in two Weeks DropDown Option
+				WebElement overview_element_filter_Drop_Upcoming = wd
+						.findElement(By.cssSelector("button[ng-click=\"hCtrl.batchFilter = 'Upcoming'\"]"));
+				TimeUnit.MILLISECONDS.sleep(100);
+				System.out.println(overview_element_filter_btn.getTagName().toString());
+				overview_element_filter_Drop_Upcoming.click();
+				TimeUnit.MILLISECONDS.sleep(900);// Time is given fro elements to load
+
+				//Re Clicks the Filter Button for the dropdown
+				overview_element_filter_btn.click();
+				TimeUnit.MILLISECONDS.sleep(200);
+
+				// All DropDown Option
+				WebElement overview_element_filter_Drop_All = wd
+						.findElement(By.cssSelector("button[ng-click=\"hCtrl.batchFilter = 'All'\"]"));
+				TimeUnit.MILLISECONDS.sleep(100);
+				System.out.println(overview_element_filter_btn.getTagName().toString());
+				overview_element_filter_Drop_All.click();
+				TimeUnit.MILLISECONDS.sleep(256);
+
 			} catch (NullPointerException e) {
+				System.out.println(e.getLocalizedMessage());
 				org.testng.Assert.fail("Get CSV for Overview has Failed to appear in Document");
 				System.out.println("Failed to find Eelement \'overview_getCSV\'");
-				
+
 			}
-			
-			TimeUnit.SECONDS.sleep(4);
+
+			TimeUnit.SECONDS.sleep(4); // to Inspect Results
 		} catch (Exception e) {
 			System.out.println("Failure due to: " + e.getLocalizedMessage());
 			org.testng.Assert.fail("Failed to Click");
@@ -87,6 +162,7 @@ public class VP_Overview_Suite {
 		}
 	}
 
+	// aria-label="Filter"
 	public void clickOverview() {
 		WebDriverWait wait = new WebDriverWait(wd, howLongtoWaitInSeconds);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(NavbarPOM.overview(wd)));
