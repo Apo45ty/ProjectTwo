7a39de3844a8e5263f154941cb10167a985e3a7b
Howard Chung <howardchung@temple.edu>
2018-05-16 11:35:41 -0400

    ctrl+shift+f a bunch of files and fixed some spelling mistakes

--- src/main/java/com/revature/http/RunTest.java
+++ src/main/java/com/revature/http/RunTest.java
@@ -16,67 +16,78 @@ import org.testng.TestNG;
  */
 public class RunTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private MyThread t;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RunTest() {
-        super();
-        t = new MyThread();
-        Thread thread = new Thread(t);
-        thread.start();
-    }
-    /**
-     * My thread class
-     * @author Elton-John
-     */
-    class MyThread implements Runnable{
+	private MyThread t;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RunTest() {
+		super();
+		t = new MyThread();
+		Thread thread = new Thread(t);
+		thread.start();
+	}
+
+	/**
+	 * My thread class
+	 * 
+	 * @author Elton-John
+	 */
+	class MyThread implements Runnable {
 		private boolean hasRequest = false;
 		private boolean done = false;
-    	@Override
+
+		@Override
 		public void run() {
-			while(!done){
-				if(hasRequest) {
+			while (!done) {
+				if (hasRequest) {
 					TestListenerAdapter adapter = new TestListenerAdapter();
 					TestNG testng = new TestNG();
-					testng.setTestClasses(new Class[] { 
-							com.revature.test.Trainer_Locations_Steps.class,
-							com.revature.test.StepsCurricula_VP.class
-					});
+					testng.setTestClasses(new Class[] { com.revature.test.Trainer_Locations_Steps.class,
+							com.revature.test.StepsCurricula_VP.class });
 					testng.addListener((ITestNGListener) adapter);
 					testng.setVerbose(-1);
 					testng.setUseDefaultListeners(false);
 					testng.run();
-					hasRequest=false;
+					hasRequest = false;
 				}
 			}
 		}
-    	public boolean getHasRequest() {
-    		return hasRequest;
-    	}
-    	public synchronized void setHasRequest(boolean hasrequest) {
-    		hasRequest = hasrequest;
-    	}
-    	public boolean isDone() {
-    		return done;
-    	}
-    	public void setDone(boolean pdone) {
-    		done = pdone;
-    	}
-    }
+
+		public boolean getHasRequest() {
+			return hasRequest;
+		}
+
+		public synchronized void setHasRequest(boolean hasrequest) {
+			hasRequest = hasrequest;
+		}
+
+		public boolean isDone() {
+			return done;
+		}
+
+		public void setDone(boolean pdone) {
+			done = pdone;
+		}
+	}
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Request-Method", "*");
 		t.setHasRequest(true);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
