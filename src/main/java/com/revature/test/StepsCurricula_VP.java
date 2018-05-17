bb781c6e868c77b940348837a0ae7269288750f4
Ramnery Lugo <ramnerylugo@Ramnerys-MacBook-Pro.local>
2018-05-17 16:04:15 -0400

    New changes.

--- src/main/java/com/revature/test/StepsCurricula_VP.java
+++ src/main/java/com/revature/test/StepsCurricula_VP.java
@@ -14,84 +14,83 @@ public class StepsCurricula_VP {
 	WebDriver driver;
 
 	@Test(priority = 1)
-	public void launches_Chrome_Browser_and_initiates_login() throws Throwable {
-		
+	public void launches_Chrome_Browser_and_initiates_login() {
+
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("user-data-dir=");
 		options.addArguments("--start-maximized");
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		driver = new ChromeDriver(options);
 		driver.get("https://dev.assignforce.revaturelabs.com");
+
+	}
+
+	@Test(priority = 2)
+	public void enters_login_credentials() {
 		driver.findElement(By.id("username")).sendKeys("test.vpoftech@revature.com.int1");
 		driver.findElement(By.id("password")).sendKeys("yuvi1712");
 		driver.findElement(By.id("password")).sendKeys(Keys.RETURN);
 	}
 
-	@Test(priority = 2)
-	public void clicks_on_curricula_tabs_and_navigates() throws Throwable {
-		TimeUnit.SECONDS.sleep(8);
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	@Test(priority = 3)
+	public void clicks_on_curricula_tabs_and_navigates() {
 
-		driver.findElement(By.xpath("//li[@name='curricula']")).click(); //Clicks Curricula Tab
-		TimeUnit.SECONDS.sleep(2);
-		driver.findElement(
-		By.xpath("//*[@id=\"view\"]/md-card/md-content/md-card[1]/md-toolbar/div/button[1]"))
-		.click(); // clicks plus sign button
-		TimeUnit.SECONDS.sleep(2);
-		driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]")).click(); //selects input texbox
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]")).clear(); //clears input textbox
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]")).sendKeys("New Core Curricula"); //Adds new curriculum name
-		
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("md-select[aria-label=\"curriculumSkills\"]")).click(); //clicks skills
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.id("select_option_32")).click(); //chooses skill
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.id("select_option_32")).sendKeys(Keys.ESCAPE);
-//		driver.findElement(By.xpath("//*[@id=\"dialogContent_6\"]/form/div[1]/h3")).click(); //clicks box
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("button[ng-click=\"saveCurriculum()\"]")).click(); //clicks save button
+		try {
+			TimeUnit.SECONDS.sleep(2);
+			driver.findElement(By.xpath("//li[@name='curricula']")).click(); // Clicks Curricula Tab
+			TimeUnit.SECONDS.sleep(2);
+			driver.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content/md-card[1]/md-toolbar/div/button[1]"))
+					.click(); // clicks plus sign button
+			TimeUnit.SECONDS.sleep(2);
+			driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]")).click(); // selects input texbox
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]")).clear(); // clears input textbox
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.cssSelector("input[aria-label=\"curriculumName\"]"))
+					.sendKeys("New Core Curricula Test2"); // Adds new curriculum name
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.cssSelector("md-select[aria-label=\"curriculumSkills\"]")).click(); // clicks skills
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.id("select_option_32")).click(); // chooses skill
+			driver.findElement(By.id("select_option_31")).click(); // chooses skill
+			driver.findElement(By.id("select_option_13")).click(); // chooses skill
+			driver.findElement(By.id("select_option_12")).click(); // chooses skill
+			driver.findElement(By.id("select_option_22")).click(); // chooses skill
+			driver.findElement(By.id("select_option_29")).click(); // chooses skill
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.id("select_option_32")).sendKeys(Keys.ESCAPE);
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.cssSelector("button[ng-click=\"saveCurriculum()\"]")).click(); // clicks save button
+		} catch (InterruptedException e) {
+		}
 
-
-		//Create Skill
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("input[ng-model=\"cCtrl.skillName\"]")).sendKeys("New Test Skill");
-		TimeUnit.SECONDS.sleep(1);
-		driver.findElement(By.cssSelector("div[ng-click=\"cCtrl.createSkill(skillForm)\")]")).click();
-
-		// driver.findElement(By.xpath("/html/body/div[3]/md-dialog/md-dialog-actions/button[2]/span")).click();
-		// driver.findElement(By.id("//*[@id=\"core\"]/md-list/md-list-item[1]/button[1]/i")).click();
-		// driver.findElement(By.xpath("//*[@id=\"select_295\"]")).click();
-		// driver.findElement(By.xpath("//*[@id=\"select_option_330\"]/div[2]")).click();
-		// driver.findElement(By.xpath("/html/body/div[3]/md-dialog")).click();
-		// driver.findElement(By.xpath("/html/body/div[3]/md-dialog/md-dialog-actions/button[2]/span")).click();
-		// driver.findElement(By.xpath("//*[@id=\"input_1\"]")).click();
-		// driver.findElement(By.xpath("//*[@id=\"input_1\"]")).sendKeys("New Skillz");
-		
-		// driver.findElement(By.xpath("//*[@id=\"skill\"]/form/div/div/div/span")).click();
-		// driver.findElement(By.id("coreArrow")).click();
-		// driver.findElement(By.id("coreArrow")).click();
 	}
 
-	// @Test(priority = 3)
-	// public void clicks_on_plus() throws Throwable {
-	// 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	// 	driver.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content/md-card[1]/md-toolbar/div/button[1]"))
-	// 	.click();
+	@Test(priority = 4)
+	public void create_new_skills() {
+		// Create Skill
 
-		// driver.findElement(By.xpath("//*[@id=\"core\"]/md-list/md-list-item[1]/button[1]/i"))
-		// 		.click(); // Curricula plus button
-	//}
+		try {
+			TimeUnit.SECONDS.sleep(1);
+			driver.findElement(By.cssSelector("input[ng-model=\"cCtrl.skillName\"]")).sendKeys("New Test Skill1");
+			TimeUnit.SECONDS.sleep(2);
+			driver.findElement(By.xpath("//*[@id=\"skill\"]/form/div/div/div")).click(); // Clicks CREATE Skill button
+		} catch (InterruptedException e) {
+		}
 
-	@Test(priority = 3)
-	public void quits_session_right_after_testing_curricula() throws Throwable {
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		TimeUnit.SECONDS.sleep(9);
-		driver.findElement(By.xpath("/html/body/div/div[1]/ng-include/div/md-content/md-nav-bar/div/nav/ul/li[9]"))
-				.click();
-		TimeUnit.SECONDS.sleep(5);
-		driver.quit();
+	}
+
+	@Test(priority = 5)
+	public void quits_session_right_after_testing_curricula() {
+		try {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			TimeUnit.SECONDS.sleep(9);
+			driver.findElement(By.xpath("/html/body/div/div[1]/ng-include/div/md-content/md-nav-bar/div/nav/ul/li[9]"))
+					.click();
+			TimeUnit.SECONDS.sleep(5);
+			driver.quit();
+		} catch (InterruptedException e) {
+		}
+
 	}
 }
