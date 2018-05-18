f991c2d9a05aac4a93d3a4bc97c1b21310db9df6
Francisco Lugo <francisco.alugojimenez@gmail.com>
2018-05-18 10:17:54 -0400

    CucumberReportsBatchProjection

---
+++ src/main/java/com/revature/cucumber/CukesReportRunner.java
@@ -0,0 +1,11 @@
+package com.revature.cucumber;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features="src/main/resources/cukesReport.feature")
+public class CukesReportRunner extends AbstractTestNGCucumberTests{
+
+	
+}
